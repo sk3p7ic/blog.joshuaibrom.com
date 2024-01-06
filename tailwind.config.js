bf9d36d61f05c32d78cc97f68d56658574cc1fec
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2024-01-06 09:54:43 -0600

    Installed needed fonts and icons.
    Installed fonts Bitter, Jura, Nunito, and Source Code Pro, all through
    fontsource variable. Additionally, Lucide Icons were installed to allow
    for a more consistent and true-to-style icon set for this website.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,ts,svelte}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito Variable', 'sans-serif'],
+        sourceCodePro: ['Source Code Pro Variable', 'monospace'],
+        jura: ['Jura Variable', 'sans-serif'],
+        bitter: ['Bitter Variable', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
