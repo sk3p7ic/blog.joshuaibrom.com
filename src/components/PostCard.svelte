b067f02e7dad163fef6bf8e97df6ce4abd623c64
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2024-01-07 11:12:22 -0600

    Styled the home page.

---
+++ src/components/PostCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import {ArrowRight} from 'lucide-svelte';
+  import {siteConfig} from '$lib/siteConfig';
+  import {type Post} from '$lib/postTypes';
+  import Tag from './posts/tag.svelte';
+  export let post: Post;
+</script>
+
+<div class="p-2 flex flex-col gap-2 bg-white border-2 border-b-4 border-e-4 border-black rounded-md">
+  <div class="flex flex-row flex-wrap justify-between items-baseline">
+    <h3 class="font-bitter font-semibold text-3xl">{post.title}</h3>
+    <span class="text-xl">{siteConfig.dateFormatter(post.date, 'short', 'en')}</span>
+  </div>
+    <div class="flex flex-row flex-wrap gap-2">
+  {#each post.tags as tn}
+    <Tag tagName={tn} />
+  {/each}
+    </div>
+  <p class="text-lg text-justify">{post.description}</p>
+  <a href={post.href} class="ms-auto flex flex-row items-center gap-1 p-1 w-fit bg-white hover:bg-violet-400 hover:text-black border-2 border-black rounded-sm hover:rounded-lg transition-all">Read post <ArrowRight size={18} /></a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
   import { siteConfig } from '$lib/siteConfig';
+  import PostCard from '../components/PostCard.svelte';
   export let data;
 </script>
 
-<div class="grow bg-pink-200 font-nunito">
-  <div>
-    <h1>{siteConfig.title}</h1>
-    <p>{siteConfig.description}</p>
+<div class="grow flex flex-col bg-amber-200 font-nunito">
+  <div class="grow flex flex-col gap-4 mx-auto p-4 h-full">
+    <div class="grow grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="flex flex-col items-center justify-center">
+        <div class="flex flex-col gap-2">
+          <h1 class="font-bitter font-bold text-6xl">{siteConfig.title}</h1>
+          <p class="font-jura text-3xl md:max-w-96">{siteConfig.description}</p>
+        </div>
+      </div>
+      <div class="p-4 flex flex-col items-center justify-center bg-pink-200 text-xl text-justify border-4 border-black rounded-lg">
+        <div class="flex flex-col gap-4">
+          <p>I'm sk3p7ic, a senior computer science student at Texas A&amp;M
+            University - San Antonio. I'm also a student worker for my
+            university's IT department, acting as a member of their web team.
+            I'm currently looking for a full-time position as a software
+            engineer or web developer, as am I heavily considering graduate
+            school.
+          </p>
+          <p>In addition to web development, I'm also interested in systems
+            programming and compiler design. I'm a huge fan of Rust, and I
+            enjoy using it for most of my personal projects. I'm also a fan
+            of C and Zig (C++ is a no-go for me, though).
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="p-4 bg-emerald-300 border-2 border-black rounded-lg">
+      <h2 class="font-bitter text-4xl font-bold">Recent Posts</h2>
+      <ul class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        {#each data.posts as post}
+          <li><PostCard post={post} /></li>
+        {/each}
+      </ul>
+    </div>
   </div>
-  <ul>
-    {#each data.posts as post}
-      <li><a href={post.href}>{post.title}</a></li>
-    {/each}
-  </ul>
 </div>
