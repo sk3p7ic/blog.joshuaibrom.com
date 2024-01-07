dc95d7a6aa6ef59b09c1459cb7357164c699773f
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2024-01-07 16:47:50 -0600

    Added page to view posts matching a tag.

---
+++ src/routes/tags/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import PostCard from '../../../components/PostCard.svelte';
+  export let data;
+</script>
+
+<div class="grow flex flex-col p-4 bg-amber-200 font-nunito">
+  <h2 class="font-bitter text-4xl font-bold">Posts Matching '{data.slug}':</h2>
+  <ul class="py-4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
+    {#each data.posts as post}
+      <li class="h-full"><PostCard {post} /></li>
+    {/each}
+  </ul>
+</div>
