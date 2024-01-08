26514c6c47e823415c0ac2e5aec830c94d77597e
Joshua Ibrom <joshuaibrom@gmail.com>
2024-01-08 07:29:38 -0600

    Fix #8. There is now a page for all posts.

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import PostCard from '../../components/PostCard.svelte';
   export let data;
-  console.log(data)
+  const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 </script>
 
 <div class="grow flex flex-col p-4 bg-amber-200 font-nunito">
   <h2 class="font-bitter text-4xl font-bold">All Posts:</h2>
-  <ul class="py-4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
-    {#each data.posts as post}
-      <li class="h-full"><PostCard {post} /></li>
-    {/each}
-  </ul>
+  {#each data.groupedPosts as grouping}
+    <div class="flex flex-col gap-2">
+      <h3 class="font-bitter text-2xl font-bold">{grouping.year}</h3>
+      <ul class="flex flex-col">
+        {#each grouping.months as month}
+          <li>
+            <h4 class="font-bitter text-xl font-bold">{month.month}: {months[parseInt(month.month) - 1]}</h4>
+            <ul class="py-4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
+              {#each month.posts as post}
+                <li class="h-full"><PostCard {post} /></li>
+              {/each}
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/each}
 </div>
--- src/routes/posts/+page.ts
+++ src/routes/posts/+page.ts
@@ -1,15 +1,39 @@
 import type { Post } from '$lib/postTypes';
 import { error, type Load } from '@sveltejs/kit';
 
+type Grouping = {
+  year: string;
+  months: {
+    month: string;
+    posts: Post[];
+  }[];
+};
+
 export const load: Load = async ({ fetch, params }) => {
   try {
     const posts: Post[] = await (await fetch(`/api/posts`)).json();
-    const groupedPosts: Record<string, Record<string, Post[]>> = {};
+    const groupedPosts: Grouping[] = [];
     posts.forEach((post) => {
       const [y, m] = post.date.split('-').slice(0, 2);
-      if (!groupedPosts[y]) groupedPosts[y] = {};
-      if (!groupedPosts[y][m]) groupedPosts[y][m] = [post];
-      else groupedPosts[y][m].push(post);
+      let yearIndex = groupedPosts.findIndex((group) => group.year === y);
+      if (yearIndex == -1) {
+        groupedPosts.push({
+          year: y,
+          months: [],
+        });
+        yearIndex = groupedPosts.length - 1;
+      }
+      let monthIndex = groupedPosts[yearIndex].months.findIndex(
+        (group) => group.month === m,
+      );
+      if (monthIndex == -1) {
+        groupedPosts[yearIndex].months.push({
+          month: m,
+          posts: [],
+        });
+        monthIndex = groupedPosts[yearIndex].months.length - 1;
+      }
+      groupedPosts[yearIndex].months[monthIndex].posts.push(post);
     });
     return {
       posts,
